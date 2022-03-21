07ae22b753fcb16da9c13663654076e65d459739
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-03-21 19:38:16 -0300

    TP Actualizado

--- TP 1/ej-1.py
+++ TP 1/ej-1.py
@@ -2,9 +2,9 @@ import re
 def contiene_mayus(cadena):
     patron = '[A-Z]'
     if re.search(patron, cadena[0]):
-        print(f'La cadena "{cadena}" contiene una mayuscula del patron: "{patron}"')
+        print(f'La cadena "{cadena}" contiene una mayuscula en su inicio del patron: "{patron}"')
     else:
-        print(f'La cadena "{cadena}" no contiene una mayuscula del patron: "{patron}"')
+        print(f'La cadena "{cadena}" no contiene una mayuscula en su inicio del patron: "{patron}"')
 
 cadena_1 = 'juan'
 cadena_2 = 'Nico'
