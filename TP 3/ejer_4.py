46b0ec1c82413c1c26870c1d497456f2627180f6
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-04-23 21:51:03 -0300

    tp1,3,4 ended

--- TP 3/ejer_4.py
+++ TP 3/ejer_4.py
@@ -1,21 +1,127 @@
 # Ejercicio 4 (en clase) - TP3 - Automata: (a|b)*(a|bb)
 
-aut = input("Enter the string: ")
-aut.re
+from asyncio import start_server
+import time
 
-dfa = {0:{'a':1, 'b':2},
-       1:{'a':1, 'b':3}, 
-       2:{'a':1, 'b':2},
-       3:{'a':1, 'b':4},
-       4:{'a':1, 'b':2}}
+print("Ingrese cualquier tipo de cadena del formato: (a|b)*abb")
+string = str(input("Ingrese cadena: "))
 
-# accepting = "2"
-initial = 0
 
-def accepts(transitions,initial,accepting,s):
-    state = initial
-    for c in s:
-        state = transitions[state][c]
-    return state in accepting
+def transition_process(string):
+    state = 0
+    # inicializo el estado en 0
+    print(f"Ha introducido: '{string}'")
+    print("INICIANDO AUTOMATA...")
+    # recorro la cadena de entrada con un for
+    for i in range(len(string)):
+        # igualo la cadena de la posicion actual a la transicion
+        transition = string[i]
+        #Para el Estado A
+        if state == 0:       
+       
+            if transition == 'a':
+                print("ESTADO: ", state)
+                state = 1
+                time.sleep(1)
+                continue
 
-print(accepts(dfa,initial,{4},aut))
+            elif transition == 'b':
+                print("ESTADO: ", state)
+                state = 2
+                time.sleep(1)
+                continue
+
+            else:
+                print("ERROR")
+                state=0
+                return
+                 
+        #Para el Estado B
+        if state == 1:   
+
+            if transition == 'a':
+                print("ESTADO: ", state)
+                state = 1
+                time.sleep(1)
+                continue
+
+            elif transition == 'b':
+                print("ESTADO: ", state)
+                state = 3  
+                time.sleep(1)           
+                continue
+
+            else:
+                print("ERROR")
+                state=0
+                return
+        
+        #Para el Estado C
+        if state == 2:
+
+            if transition == 'a':
+                print("ESTADO: ", state)
+                state = 1
+                time.sleep(1)
+                continue
+
+            elif transition == 'b':
+                print("ESTADO: ", state)
+                state = 2
+                time.sleep(1)              
+                continue
+
+            else:
+                print("ERROR")
+                state=0
+                return
+
+        #Para el Estado D
+        if state == 3:
+
+            if transition == 'a':
+                print("ESTADO: ", state)
+                state = 1
+                time.sleep(1)
+                continue
+
+            elif transition == 'b':
+                print("ESTADO: ", state)
+                state = 4
+                time.sleep(1)              
+                continue
+# break: rompe iteracion. #return:rompe funcion. #continue:en una iteracion (for-while) lee el siguiente valor.
+            else:
+                print("ERROR")
+                state=0
+                return
+        
+        #Para el Estado E
+        if state == 4:
+
+            if transition == 'a':
+                print("ESTADO: ", state)
+                state = 1
+                time.sleep(1)
+                continue
+
+            elif transition == 'b':
+                print("ESTADO: ", state)
+                state = 2
+                time.sleep(1)              
+                continue
+
+            else:
+                print("ERROR")
+                state=0
+                return
+    end_state(state)
+
+
+def end_state(state):
+    if state == 4:
+        print('Estado de Finalizacion Correcto!. Estado: ', state)
+    else:
+        print('Estado de Finalizacion INCORRECTO!. Estado: ', state)
+
+transition_process(string)
