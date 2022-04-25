82dcfe2674dc5408d4b5fb784d35e3afb0209a17
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-04-25 18:08:53 -0300

    ej url arreglado

--- TP_4/ej-1.py
+++ TP_4/ej-1.py
@@ -1,5 +1,4 @@
 import re
-from unittest import result
 
 def main():
     input_1 = input("Seleccione el dato a analizar: ")
@@ -9,19 +8,20 @@ def main():
         use_url()
     elif input_1 == 'c':
         use_ipv4()
+    elif input_1 == 'd':
+        use_password()
     else:
         print("error")
 
 def use_email():
-    fd = open("E:\Proyectos python\Automatas\TP 4\emails.txt", "w+")
+    fd = open("E:\Proyectos python\Automatas\TP_4\emails.txt", "w+")
     for i in range(0, 4):
         string = input("ingrese email: ")
-        with open("E:\Proyectos python\Automatas\TP 4\emails.txt", "a") as fd:
+        with open("E:\Proyectos python\Automatas\TP_4\emails.txt", "a") as fd:
             fd.write(str(string[i]))
             fd.write("\n")
-    fd = open("E:\Proyectos python\Automatas\TP 4\emails.txt", "r")
+    fd = open("E:\Proyectos python\Automatas\TP_4\emails.txt", "r")
     lines = fd.readlines()
-    # print(lines)
 
     patron = '[a-z A-Z]'
     patron_2 = '[a-z A-Z 0-9 _ . - yahoo gmail hotmail um outlook fr uk de be es]'
@@ -35,29 +35,38 @@ def use_email():
             print("error")
 
 def use_url():
-    fs = open(r"E:\Proyectos python\Automatas\TP 4\url.txt", "w+")
-    for i in range(0, 4):
+    fs = open(r"E:\Proyectos python\Automatas\TP_4\url.txt", "w+")
+    for i in range(0, 2):
         string = input("ingrese url: ")
-        with open(r"E:\Proyectos python\Automatas\TP 4\url.txt", "a") as fs:
-            fs.write(str(string[i]))
+        with open(r"E:\Proyectos python\Automatas\TP_4\url.txt", "a") as fs:
+            fs.write(str(string))
             fs.write("\n")
-    fs = open(r"E:\Proyectos python\Automatas\TP 4\url.txt", "r")
+            fs.close()
+    fs = open(r"E:\Proyectos python\Automatas\TP_4\url.txt", "r")
     lines = fs.readlines()
-    # print(lines)
 
-    patron = '[a-z A-Z . 0-9]'
-    patron_opt = '[http https www : /]'
+    # declaro regex
+    regex = re.compile('''((https|http)\:\/\/?|(www\.)?)(([a-zA-Z0-9])+|([-_]))(\.)(net|com|org|onion)([\.])?(ar|edu)?''')
     
+    # creo listas
+    valid = []
+    invalid = []
 
-    for i in range(len(lines)):
-        if re.search(patron, lines[i]):
-            if re.search(patron_opt, lines[i]):
-                print("url correcto con protocolo incluido")
-            else:
-                print("url correcto")
+    # uso un for para recorrer las lineas del archivo
+    for line in lines:
+        line = line.rstrip()
+        result = regex.search(line)
+        # busco en la regex el patron indicado y agrego a la lista si es valido
+        if result:
+            valid.append(line)    
         else:
-            print("error en la url")
-    
+            invalid.append(line)
+    # printeo los resultados
+    print("URLs VALIDAS")
+    print(valid)
+    print("URLs INVALIDAS")
+    print(invalid)
+
 
 def use_ipv4():
     fs = open("/home/aaron/Documents/Automatas/Automatas/TP_4/ipv4.txt", "w+")
@@ -89,26 +98,6 @@ def use_ipv4():
     print("IPs INVALIDAS")
     print(invalid)
 
-#([0-2][0-5][0-5])
-    # [0-9]|[0-9]{2}|[0-2][0-5][0-9]
 
 if __name__ == '__main__':
     main()
-
-
-
-
-# if re.search(patron, lines[0]):
-    #     print("el email es correcto")
-    # elif re.search(patron_2, lines[0]):
-    #     print("el email es correcto")
-    # elif re.search(patron_domain, lines[0]):
-    #     print("el email es correcto")
-    # elif re.search(patron_country, lines[0]):
-    #     print("el email es correcto")
-    # else:
-    #     print("el email no corresponde con la sintaxis")
-    # if re.search(patron, lines[0]) and re.search(patron_2, lines[0]) and re.search(patron_domain, lines[0]) and re.search(patron_country, lines[0]):
-    #     print("el email es correcto")
-    # else:
-    #     print("el email no corresponde con la sintaxis")
