6e3346d8b504eeb00a8444fd83aca3028e61267e
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-04-24 23:02:33 -0300

    ej2 agregado

--- TP_3/ej-2.py
+++ TP_3/ej-2.py
@@ -1,19 +1,111 @@
 # Ejercicio 2 - TP3 - Automata: (aa|b)*(a|bb)*
+import time
 
-aut = input(str("Enter the string: "))
+def main():
+    print("Ingrese cualquier tipo de cadena del formato: (aa|b)*(a|bb)*")
+    string = str(input("Ingrese cadena: "))
+    transition_process(string)
 
-dfa = {0:{'a':1, 'b':2},
-       1:{'a':1, 'b':3}, 
-       2:{'a':3, 'b':2},
-       3:{'a':3, 'b':3}}
+def transition_process(string):
+    # inicializo el estado en 0
+    state = 0
+    print(f"Ha introducido: '{string}'")
+    print("INICIANDO AUTOMATA...")
+    # recorro la cadena de entrada con un for
+    for i in range(len(string)):
+        # igualo la cadena de la posicion actual a la transicion
+        transition = string[i]
+        #Para el Estado A
+        if state == 0:       
+       
+            if transition == 'a':
+                print("ESTADO: ", state)
+                state = 1
+                time.sleep(1)
+                continue
+            elif transition == 'b':
+                print("ESTADO: ", state)
+                state = 2
+                time.sleep(1)
+                continue
+            else:
+                print("ERROR")
+                state=0
+                return
+                 
+        #Para el Estado B
+        if state == 1:   
 
-# accepting = "2"
-initial = 0
+            if transition == 'a':
+                print("ESTADO: ", state)
+                state = 3
+                time.sleep(1)
+                continue
+            elif transition == 'b':
+                print("ESTADO: ", state)
+                state = 2                
+                time.sleep(1)           
+                continue
+            else:
+                print("ERROR")
+                state=0
+                return
+        
+        #Para el Estado C
+        if state == 2:
 
-def accepts(transitions,initial,accepting,s):
-    state = initial
-    for c in s:
-        state = transitions[state][c]
-    return state in accepting
+            if transition == 'a':
+                print("ESTADO: ", state)
+                state = 1
+                time.sleep(1)
+                continue
+            elif transition == 'b':
+                print("ESTADO: ", state)
+                state = 4
+                time.sleep(1)              
+                continue
+            else:
+                print("ERROR")
+                state=0
+                return
 
-print(accepts(dfa,initial,{1,2,3},aut))
+        #Para el Estado D
+        if state == 3:
+
+            if transition == 'a':
+                print("ESTADO: ", state)
+                state = 3
+                time.sleep(1)
+                continue
+            elif transition == 'b':
+                print("ESTADO: ", state)
+                state = 2
+                time.sleep(1)              
+                continue
+            else:
+                print("ERROR")
+                state=0
+                return
+
+        #Para el Estado E
+        if state == 4:
+
+            if transition == 'a':
+                print("ESTADO: ", state)
+                state = 1
+                time.sleep(1)
+                continue
+            elif transition == 'b':
+                print("ESTADO: ", state)
+                state = 4
+                time.sleep(1)              
+                continue
+            else:
+                print("ERROR")
+                state=0
+                return
+    
+    print("Estado de aceptacion alcanzado:", state)
+
+if __name__ == '__main__':
+    main()
