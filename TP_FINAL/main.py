1d414f6d13164bf54b522e66e4599f7e6e993d4c
Nicolás Mayoral <n.mayoral@alumno.um.edu.ar>
2022-06-05 11:28:32 -0300

    menu y clase user agregada

---
+++ TP_FINAL/main.py
@@ -0,0 +1,41 @@
+import const as cs
+import functions as ft
+import time
+import user as us
+
+
+def main():
+    user = us.User()
+    while True:
+        print(cs.MENU)
+        inp = input("")
+        inp = inp.lower()
+        if inp == 'a':
+            user.list_session_id()
+            ft.return_menu()
+        elif inp == 'b':
+            ft.return_menu()
+        elif inp == 'c':
+            ft.return_menu()
+        elif inp == 'd':
+            ft.return_menu()
+        elif inp == 'e':
+            ft.return_menu()
+        elif inp == 'f':
+            ft.return_menu()
+        elif inp == 'g':
+            ft.return_menu()
+        elif inp == 'h':
+            print(cs.HELP)
+            time.sleep(3)
+            ft.return_menu()
+        elif inp == 'i':
+            ft.return_menu()
+        elif inp == 'q':
+            ft.exit_program()
+            break
+        else:
+            ft.error_menu()
+
+if __name__ == '__main__':
+    main()
---
+++ TP_FINAL/user.py
@@ -0,0 +1,9 @@
+import const as cs
+import time
+
+class User:
+
+    def list_session_id(self):
+        print(cs.JUMP_LINE)
+        print("Aca voy a listar lo del ejercicio a)")
+        time.sleep(1)
