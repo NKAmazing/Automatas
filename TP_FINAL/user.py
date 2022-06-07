675a3765c785999796f1d925184007f5812cf7be
Nicolás Mayoral <n.mayoral@alumno.um.edu.ar>
2022-06-06 21:56:16 -0300

    empezado id session, revisar loc

--- TP_FINAL/user.py
+++ TP_FINAL/user.py
@@ -1,9 +1,20 @@
 import const as cs
 import time
+import pandas as pd
 
 class User:
 
+    def operate_csv_file(self):
+        data = pd.read_csv(cs.PATH_CSV)
+        df = pd.DataFrame(data) 
+        return df
+
     def list_session_id(self):
         print(cs.JUMP_LINE)
+        df = self.operate_csv_file()
+        inp = input(cs.UN_INP)
+        if inp in df.values:
+            df2 = df.loc["ID Conexion unico", "Usuario"]
+            # revisar esto para poder listar con loc
         print("Aca voy a listar lo del ejercicio a)")
         time.sleep(1)
